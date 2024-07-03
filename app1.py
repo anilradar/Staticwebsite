4028e7d5db258dd6007504428e7bcccff6404577
anilradar <anilradar099@gmail.com>
2024-07-03 12:42:44 +0530

    added app1.py file

---
+++ app1.py
@@ -0,0 +1,13 @@
+c = 10
+d = 20
+sum = c + d
+Diff = d - c
+# product = a * b
+# Div = b // a
+# Mod = b % a
+
+print("The sum of c and d is: ", sum)
+print("The difference of d and c is: ", Diff)
+# print("The product of b and a is: ", product)
+# print("The division of a and b is: ", Div)
+# print("The modulus of a and b is: ", Mod)
