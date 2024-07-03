c56b298de973985a002fae0286e3f1e891a86a3a
anilradar <anilradar099@gmail.com>
2024-07-03 12:50:57 +0530

    addedd div changes

--- app1.py
+++ app1.py
@@ -3,11 +3,11 @@ d = 20
 sum = c + d
 Diff = d - c
 product = c * d
-# Div = b // a
+Div = d // c
 # Mod = b % a
 
 print("The sum of c and d is: ", sum)
 print("The difference of d and c is: ", Diff)
 print("The product of c and d is: ", product)
-# print("The division of a and b is: ", Div)
+print("The division of d and c is: ", Div)
 # print("The modulus of a and b is: ", Mod)
